9cd3ab184cc46eb19ad7fb137134ba1684d900c2
Roberto Fustinoni <75791181+Pygeddon@users.noreply.github.com>
2024-12-21 11:00:59 +0100

    Delete formLesson directory

--- formLesson/formLesson1/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    content = forms.CharField(widget=forms.Textarea( attrs={'placeholder': 'Your message'}))
